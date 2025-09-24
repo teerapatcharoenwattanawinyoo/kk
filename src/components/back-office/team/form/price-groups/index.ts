db6ab1f9a12a7ed6fbea20dc3887d5479708c1e6
Teerapat Charoenwattanawinyoo <1167304030477@mail.rmutt.ac.th>
2025-09-24 16:11:34 +0700

    refactor: centralize price group form schemas

--- src/components/back-office/team/form/price-groups/index.ts
+++ src/components/back-office/team/form/price-groups/index.ts
@@ -1,10 +1,22 @@
 export { default as PriceGroupForm } from './price-group-form'
+export {
+  feeFormSchema,
+  formSchema,
+  modeSchema,
+  priceFormSchema,
+  priceGroupFormInitialDataSchema,
+  priceGroupSubmissionSchema,
+  priceTypeSchema,
+  statusTypeSchema,
+} from './_schemas/price-group'
 export type {
   FeeFormData,
   FormData,
   Mode,
   PriceFormData,
+  PriceGroupFormInitialData,
   PriceGroupFormProps,
+  PriceGroupSubmissionData,
   PriceType,
   StatusType,
-} from './price-group-form'
+} from './_schemas/price-group'

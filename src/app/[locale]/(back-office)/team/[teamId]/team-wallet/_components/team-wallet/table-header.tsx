18e5753f04122946549bde84baeb276645c84f7c
Teerapat Charoenwattanawinyoo <1167304030477@mail.rmutt.ac.th>
2025-10-01 02:52:41 +0700

    refactor: build charge sessions table with tanstack

--- src/app/[locale]/(back-office)/team/[teamId]/team-wallet/_components/team-wallet/table-header.tsx
+++ src/app/[locale]/(back-office)/team/[teamId]/team-wallet/_components/team-wallet/table-header.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { ChevronDown, Search } from 'lucide-react'
 
 interface TableHeaderProps {
   searchQuery: string
@@ -25,20 +26,7 @@ export function TableHeader({
             className="h-10 bg-[#ECF2F8] pl-4 pr-10 placeholder:font-medium placeholder:text-[#A1B1D1]"
           />
           <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-            <svg
-              className="h-4 w-4 text-[#A1B1D1]"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="none"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+            <Search className="h-4 w-4 text-[#A1B1D1]" />
           </div>
         </div>
         <Button
@@ -47,20 +35,7 @@ export function TableHeader({
           className="h-10 gap-1 whitespace-nowrap bg-[#ECF2F8] text-xs sm:text-sm"
         >
           <span className="text-[#A1B1D1]">Filter by Status</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="h-4 w-4 text-[#A1B1D1]"
-          >
-            <path d="m6 9 6 6 6-6" />
-          </svg>
+          <ChevronDown className="h-4 w-4 text-[#A1B1D1]" />
         </Button>
       </div>
     </div>

4e216a3ede90cb87f7a446ad804f07a178f5bf6a
DevKim <khimnk635@gmail.com>
2025-10-01 10:37:38 +0700

    WAKIM |

--- src/app/[locale]/(back-office)/team/[teamId]/team-wallet/_components/team-wallet/charge-sessions-table.tsx
+++ src/app/[locale]/(back-office)/team/[teamId]/team-wallet/_components/team-wallet/charge-sessions-table.tsx
@@ -246,7 +246,7 @@ export function ChargeSessionsTable({
                   <TableHead
                     key={header.id}
                     className={cn(
-                      'bg-[#F7FAFC] px-4 py-3 text-center text-[10px] font-semibold uppercase text-primary',
+                      'bg-background px-4 py-3 text-center text-[10px] font-semibold uppercase text-primary',
                       (header.column.columnDef as ChargeSessionsColumn).meta?.className,
                     )}
                   >
